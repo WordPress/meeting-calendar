029dcd215aded407922c19eca2575d35b23c6c18
dufresnesteven <steve.dufresne@automattic.com>
2020-02-19 08:42:41 +0900

    Rename component files.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { registerBlockType } from '@wordpress/blocks'
 /**
  * Internal dependencies
  */
-import editView from './edit';
-import saveView from './save';
+import edit from './edit';
+import save from './save';
 
 registerBlockType( 'a8c-meeting-calendar/main', {
 	title: 'Meeting Calendar',
 	icon: 'calendar',
 	category: 'widgets',
 	attributes: {},
-	edit: editView,
-	save: saveView,
+	edit,
+	save,
 } );
