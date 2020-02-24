3cd1062fe452b33ee0b7088f2a8e91dde4621def
dufresnesteven <steve.dufresne@automattic.com>
2020-02-24 12:43:33 +0900

    Add a max event for a day.

--- src/frontend/calendar/cell.js
+++ src/frontend/calendar/cell.js
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import { format } from '@wordpress/date';
+import { __ } from '@wordpress/i18n';
 
 function CalendarCell( { blank = false, year, month, day, events } ) {
 	if ( blank ) {
@@ -11,6 +12,7 @@ function CalendarCell( { blank = false, year, month, day, events } ) {
 	const date = new Date( year, month, day );
 	const key = format( 'Y-m-d', date );
 	const dayEvents = events[ key ] || [];
+	const maxEventsToDisplay = 2;
 
 	return (
 		<td className="wporg-meeting-calendar__cell">
@@ -20,7 +22,7 @@ function CalendarCell( { blank = false, year, month, day, events } ) {
 				</span>
 				<span aria-hidden>{ day }</span>
 			</strong>
-			{ dayEvents.map( ( e ) => {
+			{ dayEvents.slice( 0, maxEventsToDisplay ).map( ( e ) => {
 				// Get the event date + time from the event using RFC3339 for `format`.
 				// @todo Get the recurring event time, not just first instance.
 				const eventDate = e.meta.start_date + 'T' + e.meta.time + 'Z';
@@ -31,6 +33,20 @@ function CalendarCell( { blank = false, year, month, day, events } ) {
 					</h3>
 				);
 			} ) }
+
+			{ dayEvents.length > maxEventsToDisplay && (
+				<a
+					href={ '/day/' + date.toISOString() }
+					onClick={ ( e ) => {
+						e.preventDefault();
+
+						// @todo trigger list view
+					} }
+				>
+					{ dayEvents.length - maxEventsToDisplay }
+					{ __( 'more', 'wporg' ) }
+				</a>
+			) }
 		</td>
 	);
 }
