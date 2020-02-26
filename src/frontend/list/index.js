25164c49d2e55ab9025563857631792f98b6b903
dufresnesteven <steve.dufresne@automattic.com>
2020-02-26 10:56:17 +0900

    Small cleanup of listItem naming conventions and linting issues in list view.

--- src/frontend/list/index.js
+++ src/frontend/list/index.js
@@ -8,31 +8,32 @@ import { format } from '@wordpress/date';
  */
 import { getRows, getSortedEvents } from '../calendar/utils';
 import { useEvents } from '../calendar/event-context';
-import ListItem from './listItem';
+import ListItem from './list-item';
 
-function List( { month, year, week } ) {
+function List( { month, year } ) {
 	const rows = getRows( year, month );
 	const events = getSortedEvents( useEvents() );
 	const allDays = rows.flat().filter( ( i ) => ! i.blank );
 
-	const days = allDays.map( ( row, i ) => {
-		const date = new Date( row.year, row.month, row.day );
-		const key = format( 'Y-m-d', date );
-		const dayEvents = events[ key ] || [];
+	const days = allDays
+		.map( ( row, i ) => {
+			const date = new Date( row.year, row.month, row.day );
+			const key = format( 'Y-m-d', date );
+			const dayEvents = events[ key ] || [];
 
-		if ( ! dayEvents.length ) {
-			return;
-		}
+			if ( ! dayEvents.length ) {
+				return null;
+			}
 
-		return (
-			<ListItem
-				key={ `row-${ i }` }
-				key={ key }
-				date={ date }
-				events={ dayEvents }
-			/>
-		);
-	} ).filter( i => !! i );
+			return (
+				<ListItem
+					key={ `row-${ i }` }
+					date={ date }
+					events={ dayEvents }
+				/>
+			);
+		} )
+		.filter( ( i ) => !! i );
 
 	if ( ! days.length ) {
 		return <div>No Events Scheduled</div>;
