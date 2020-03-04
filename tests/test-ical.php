9a1333c534a70e89322501d4643ba4adf85ba083
tellyworth <alex@automattic.com>
2020-03-04 16:30:30 +1100

    Simplify the unit test

--- tests/test-ical.php
+++ tests/test-ical.php
@@ -107,49 +107,21 @@ EOF;
 	}
 
 	public function test_get_ical_with_cancellation() {
-		$posts = WordPressdotorg\Meeting_Calendar\ICS\get_meeting_posts();
+		$posts = WordPressdotorg\Meeting_Calendar\ICS\get_meeting_posts( 'Team-A' );
 		// Cancel the second occurrence of the weekly meeting
-		$occurrences = Meeting_Post_Type::getInstance()->get_future_occurrences( get_post( $this->meeting_ids[0] ), null, null );
+		$occurrences = Meeting_Post_Type::getInstance()->get_future_occurrences( get_post( $posts[0]->ID ), null, null );
 		$this->assertGreaterThan( 0, Meeting_Post_Type::getInstance()->cancel_meeting( array( 
-			'meeting_id' => $this->meeting_ids[0], 
+			'meeting_id' => $posts[0]->ID, 
 			'date'       => $occurrences[1] 
 		) ) );
 
 		$ical_feed = WordPressdotorg\Meeting_Calendar\ICS\Generator\generate( $posts );
 
-		$expected_posts = '';
 		Meeting_Post_Type::getInstance()->meeting_set_next_meeting( $posts, new WP_Query( array('post_type' => 'meeting', 'nopaging' => true ) ) );	
 		foreach ( $posts as $i => $post ) {
 			$post->datetime = strftime( '%Y%m%dT%H%M%SZ', strtotime( "{$post->next_date} {$post->time} GMT" ) );
 			$post->end_datetime = strftime( '%Y%m%dT%H%M%SZ', strtotime( "{$post->next_date} {$post->time} GMT +1 hour" ) );
-			if ( $post->ID === $this->meeting_ids[0] ) {
-				$post->rrule = 'FREQ=WEEKLY';
-				$post->rrule .= "\n" . 'EXDATE:' . str_replace( '-', '', $occurrences[1] );
-			} elseif ( $post->ID === $this->meeting_ids[1] )
-				$post->rrule = 'FREQ=MONTHLY';
-			elseif ( $post->ID === $this->meeting_ids[2] )
-				$post->rrule = 'FREQ=MONTHLY;BYDAY=3WE';
-			else
-				$post->rrule = 'FIX THE TESTS ALEX';
-
-			$expected_posts .= <<<EOF
-BEGIN:VEVENT
-UID:{$post->ID}
-DTSTAMP:{$post->datetime}
-DTSTART;VALUE=DATE:{$post->datetime}
-DTEND;VALUE=DATE:{$post->end_datetime}
-CATEGORIES:WordPress
-ORGANIZER;CN=WordPress {$post->team} Team:mailto:mail@example.com
-SUMMARY:{$post->team}: {$post->post_title}
-SEQUENCE:0
-STATUS:CONFIRMED
-TRANSP:OPAQUE
-LOCATION:#meta channel on Slack
-DESCRIPTION:Slack channel link: https://wordpress.slack.com/messages/#meta\\nFor more information visit wordpress.org
-RRULE:{$post->rrule}
-END:VEVENT
-
-EOF;
+			$post->exdate = str_replace( '-', '', $occurrences[1] );
 		}
 
 		$expected = <<<EOF
@@ -158,7 +130,23 @@ VERSION:2.0
 PRODID:-//Make WordPress//Meeting Events Calendar//EN
 METHOD:PUBLISH
 CALSCALE:GREGORIAN
-{$expected_posts}END:VCALENDAR
+BEGIN:VEVENT
+UID:49
+DTSTAMP:{$posts[0]->datetime}
+DTSTART;VALUE=DATE:{$posts[0]->datetime}
+DTEND;VALUE=DATE:{$posts[0]->end_datetime}
+CATEGORIES:WordPress
+ORGANIZER;CN=WordPress Team-A Team:mailto:mail@example.com
+SUMMARY:Team-A: A weekly meeting
+SEQUENCE:0
+STATUS:CONFIRMED
+TRANSP:OPAQUE
+LOCATION:#meta channel on Slack
+DESCRIPTION:Slack channel link: https://wordpress.slack.com/messages/#meta\\nFor more information visit wordpress.org
+RRULE:FREQ=WEEKLY
+EXDATE:{$posts[0]->exdate}
+END:VEVENT
+END:VCALENDAR
 EOF;
 
 		$this->assertEquals( preg_split('/\r\n|\r|\n/',$expected), preg_split('/\r\n|\r|\n/',$ical_feed) );
