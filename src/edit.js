aa23a1f725e661681b278d954ece75d0d8478108
dufresnesteven <steve.dufresne@automattic.com>
2020-03-05 10:11:12 +0900

    Add a grey block for the editor preview.

--- src/edit.js
+++ src/edit.js
@@ -1,5 +1,16 @@
-const EditView = () => {
-	return <div>The front end is alive.</div>;
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Icon } from '@wordpress/components';
+
+const EditView = ( { className } ) => {
+	return (
+		<div className={ className }>
+			<Icon icon="calendar" />
+			<h6>{ __( 'Meeting Calendar', 'wporg' ) }</h6>
+		</div>
+	);
 };
 
 export default EditView;
