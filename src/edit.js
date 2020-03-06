6f3aa3be9e7e7273276af67864c1540c08460c18
Steven Dufresne <steve.dufresne@automattic.com>
2020-03-07 06:04:08 +0900

    Use the wordpress placeholder component for edit. (#60)
    * Use the wordpress placeholder component for edit.
    
    * Run `format:js`
    
    Co-authored-by: Kelly Dwan <ryelle@users.noreply.github.com>

--- src/edit.js
+++ src/edit.js
@@ -1,5 +1,13 @@
-const EditView = () => {
-	return <div>The front end is alive.</div>;
-};
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Placeholder } from '@wordpress/components';
 
+const EditView = () => (
+	<Placeholder
+		icon="calendar"
+		label={ __( 'Go to "Meetings" to add or edit your meetings', 'wporg' ) }
+	/>
+);
 export default EditView;
