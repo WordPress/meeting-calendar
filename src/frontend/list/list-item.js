b725dc424c40ecbcb6ac7b3cf72b3a9585c2f5e0
dufresnesteven <steve.dufresne@automattic.com>
2020-03-09 09:41:46 +0900

    Some some updates.

--- src/frontend/list/list-item.js
+++ src/frontend/list/list-item.js
@@ -1,66 +1,70 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { format } from '@wordpress/date';
+import { __ } from "@wordpress/i18n";
+import { format } from "@wordpress/date";
 
 /**
  * Internal dependencies
  */
 import {
-	getTeamClass,
-	getSlackLink,
-	getFrequencyLabel,
-} from '../calendar/utils';
-import { useEvents } from '../store/event-context';
+  getTeamClass,
+  getSlackLink,
+  getFrequencyLabel
+} from "../calendar/utils";
+import { useEvents } from "../store/event-context";
 
-function ListItem( { date, events } ) {
-	const { setTeam } = useEvents();
+function ListItem({ date, events }) {
+  const { setTeam } = useEvents();
 
-	return (
-		<li key={ `row-${ date }` }>
-			<strong className="wporg-meeting-calendar__list-title">
-				<span>{ format( 'l - F j, Y', date ) }</span>
-			</strong>
+  return (
+    <li key={`row-${date}`}>
+      <strong className="wporg-meeting-calendar__list-title">
+        <span>{format("l - F j, Y", date)}</span>
+      </strong>
 
-			{ events.map( ( event ) => {
-				return (
-					<article
-						className="wporg-meeting-calendar__list-event"
-						key={ event.instance_id }
-					>
-						<div>{ format( 'g:i a: ', event.datetime ) }</div>
-						<div>
-							<a
-								className={
-									'wporg-meeting-calendar__list-event-team ' +
-									getTeamClass( event.team )
-								}
-								href={ `#${ event.team.toLowerCase() }` }
-								onClick={ ( clickEvent ) => {
-									clickEvent.preventDefault();
-									setTeam( event.team );
-								} }
-							>
-								{ event.team }
-							</a>
-							<h3 className="wporg-meeting-calendar__list-event-title">
-								<a href={ event.link }>{ event.title }</a>
-							</h3>
-							<p className="wporg-meeting-calendar__list-event-copy">
-								{ __( 'Meets: ', 'wporg' ) }
-								{ getFrequencyLabel( event ) }
-							</p>
-							<p className="wporg-meeting-calendar__list-event-copy">
-								{ __( 'Location: ', 'wporg' ) }
-								{ getSlackLink( event.location ) }
-							</p>
-						</div>
-					</article>
-				);
-			} ) }
-		</li>
-	);
+      {events.map(event => {
+        return (
+          <article
+            className="wporg-meeting-calendar__list-event"
+            key={event.instance_id}
+          >
+            <div>{format("g:i a: ", event.datetime)}</div>
+            <div className="wporg-meeting-calendar__list-event-details">
+              <div>
+                <a
+                  className={
+                    "wporg-meeting-calendar__list-event-team " +
+                    getTeamClass(event.team)
+                  }
+                  href={`#${event.team.toLowerCase()}`}
+                  onClick={clickEvent => {
+                    clickEvent.preventDefault();
+                    setTeam(event.team);
+                  }}
+                >
+                  {event.team}
+                </a>
+              </div>
+              <div>
+                <h3 className="wporg-meeting-calendar__list-event-title">
+                  <a href={event.link}>{event.title}</a>
+                </h3>
+                <p className="wporg-meeting-calendar__list-event-copy">
+                  {__("Meets: ", "wporg")}
+                  {getFrequencyLabel(event)}
+                </p>
+                <p className="wporg-meeting-calendar__list-event-copy">
+                  {__("Location: ", "wporg")}
+                  {getSlackLink(event.location)}
+                </p>
+              </div>
+            </div>
+          </article>
+        );
+      })}
+    </li>
+  );
 }
 
 export default ListItem;
