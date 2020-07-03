91a4243be8d66d26b40683eef0b8d7d73a057617
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-07-02 20:10:02 -0700

    Update bootstrap.php
    Syncing changes made in https://dotorg.trac.wordpress.org/changeset/16222

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,28 +5,18 @@
  * @package Meeting_Calendar
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
+namespace WordPressdotorg\Meeting_Calendar\Tests;
 
-if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+if ( 'cli' !== php_sapi_name() ) {
+	return;
 }
-
-if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	exit( 1 );
-}
-
-// Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
 
 /**
  * Manually load the plugin being tested.
  */
-function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/plugin.php';
-	require dirname( dirname( __FILE__ ) ) . '/includes/wporg-meeting-install.php';
+function manually_load_plugin() {
+	require_once dirname( __FILE__ ) . '/../plugin.php';
+	require_once dirname( __FILE__ ) . '/../includes/wporg-meeting-install.php';
 }
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-// Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+tests_add_filter( 'muplugins_loaded', __NAMESPACE__ . '\manually_load_plugin' );
