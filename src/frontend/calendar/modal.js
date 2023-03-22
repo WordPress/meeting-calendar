756bc96d90abb588156920f508714c1d3204895b
ren <18050944+renintw@users.noreply.github.com>
2023-03-23 03:55:29 +0800

    Add View Recording link to modal

--- src/frontend/calendar/modal.js
+++ src/frontend/calendar/modal.js
@@ -73,6 +73,13 @@ function EventModal( { event, onRequestClose } ) {
 					{ __( 'Add to Google Calendar', 'wporg-meeting-calendar' ) }
 				</a>
 			</p>
+			{ !! event.wptv_url && (
+				<p>
+					<a href={ event.wptv_url }>
+						{ __( 'View Recording', 'wporg-meeting-calendar' ) }
+					</a>
+				</p>
+			) }
 		</Modal>
 	);
 }
