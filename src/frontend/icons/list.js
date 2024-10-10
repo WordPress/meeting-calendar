c45c2019ce36c2498715ae17d99494f401961beb
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-10-10 13:20:49 +1300

    Use CSS for icon colors

--- src/frontend/icons/list.js
+++ src/frontend/icons/list.js
@@ -3,7 +3,7 @@
  */
 import { SVG, Path } from '@wordpress/primitives';
 
-export default function ( { fill = 'white' } ) {
+export default function () {
 	return (
 		<SVG
 			width="24"
@@ -16,7 +16,6 @@ export default function ( { fill = 'white' } ) {
 				fillRule="evenodd"
 				clipRule="evenodd"
 				d="M6 10C7.10457 10 8 9.10457 8 8C8 6.89543 7.10457 6 6 6C4.89543 6 4 6.89543 4 8C4 9.10457 4.89543 10 6 10ZM20 8.75H11.1111V7.25H20V8.75ZM20 15.75H11.1111V14.25H20V15.75ZM8 15C8 16.1046 7.10457 17 6 17C4.89543 17 4 16.1046 4 15C4 13.8954 4.89543 13 6 13C7.10457 13 8 13.8954 8 15Z"
-				fill={ fill }
 			/>
 		</SVG>
 	);
